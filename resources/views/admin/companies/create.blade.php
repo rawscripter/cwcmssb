b80b80a0205ac42992ff015450c1241fd96e22d2
rawscripter <rawscripterx@gmail.com>
2022-05-22 19:53:53 +0600

    cwmssb

---
+++ resources/views/admin/companies/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.dashboard')
+@section('content')
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('companies.store') }}" class="user">
+                <div class="row">
+                    <div class="col-sm-6 mb-3 mb-sm-0">
+                        <input type="text" class="form-control form-control-user" id="exampleFirstName"
+                            placeholder="First Name">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
